699a551e9cda30931dc842aa432d4834998166e0
sewwwer <www.svr@gmail.com>
2015-08-27 10:59:26 +0300

    try block instead of DI

---
+++ Block/Form/TMRobokassa.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\TMRobokassa\Block\Form;
+
+/**
+ * Abstract class for Cash On Delivery and Bank Transfer payment method form
+ */
+abstract class TMRobokassaForm extends \Magento\Payment\Block\Form
+{
+    /**
+     * Instructions text
+     *
+     * @var string
+     */
+    protected $_instructions;
+
+
+    protected $_template = 'form/tmrobokassa.phtml';
+
+    /**
+     * Get instructions text from config
+     *
+     * @return null|string
+     */
+    public function getInstructions()
+    {
+        if ($this->_instructions === null) {
+            /** @var \Magento\Payment\Model\Method\AbstractMethod $method */
+            $method = $this->getMethod();
+            $this->_instructions = $method->getConfigData('instructions');
+        }
+        return $this->_instructions;
+    }
+}
