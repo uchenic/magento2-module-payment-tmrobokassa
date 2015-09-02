28e15aa0c81ab1c189bc8099b310b1ce12cb3a21
sewwwer <www.svr@gmail.com>
2015-09-02 14:42:47 +0300

    add controller fix

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -4,7 +4,7 @@
  * See COPYING.txt for license details.
  */
 
-namespace Magento\SampleNewPage\Controller\Index;
+namespace Magento\TMRobokassa\Controller\Index;
 
 /**
  * Responsible for loading page content.
